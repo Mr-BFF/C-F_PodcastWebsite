47062352fafa989f19d3b044c9aeb95183b119df
Angelo Jackson <78512522+AngeloJacksonED@users.noreply.github.com>
2021-08-17 17:25:04 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $(".dropdown-trigger").dropdown();
-
+    $(".dropdown-trigger").dropdown1();
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
