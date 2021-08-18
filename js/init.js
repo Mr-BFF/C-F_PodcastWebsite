f27423a541daaf54403826487e27c6696abfc5c6
Angelo Jackson <78512522+AngeloJacksonED@users.noreply.github.com>
2021-08-17 17:36:44 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -5,6 +5,7 @@
     $('.parallax').parallax();
     $(".dropdown-trigger").dropdown();
     $(".dropdown-trigger").dropdown1();
+    var instance = M.Dropdown.getInstance(elem);
     
   }); // end of document ready
 })(jQuery); // end of jQuery name space
